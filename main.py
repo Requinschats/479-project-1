98836c4be42352cce83e3df26ca2ea2f038e5f4e
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-09-24 15:18:26 -0400

    read files

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from pathlib import Path
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    for fileName in os.listdir("reuters21578"):
+        if fileName != "reut2-017.sgm":
+            file_content = Path('reuters21578/' + fileName).read_text()
+            print(file_content)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
